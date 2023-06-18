b7f5c318387947578d416fa0a3ac57006b75dc2d
Andre Luis <dejunior10@gmail.com>
2023-06-17 23:30:58 -0300

    Add the sklearn and xgboost libraries

--- handler.py
+++ handler.py
@@ -4,6 +4,7 @@ import pandas as pd
 from flask import Flask, request, Response
 from rossmann.Rossmann import Rossmann
 
+
 # loading model
 model = pickle.load( open( 'model/model_rossmann.pkl', 'rb') )
                           
